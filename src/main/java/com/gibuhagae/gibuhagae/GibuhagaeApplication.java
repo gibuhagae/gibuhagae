3759dcdfdd8926f33a47842441715063fca388f7
HAHYEONLEE <adr1025@naver.com>
2023-09-25 11:44:17 +0900

    commit init

---
+++ src/main/java/com/gibuhagae/gibuhagae/GibuhagaeApplication.java
@@ -0,0 +1,13 @@
+package com.gibuhagae.gibuhagae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibuhagaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibuhagaeApplication.class, args);
+	}
+
+}
