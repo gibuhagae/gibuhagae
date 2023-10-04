c70bc35c379a4bac2defd8f26004a8e19396dbe1
woose0k <woose0k@gmail.com>
2023-10-04 17:51:41 +0900

    first merge

--- src/main/java/com/gibuhagae/gibuhagae/GibuhagaeApplication.java
+++ src/main/java/com/gibuhagae/gibuhagae/GibuhagaeApplication.java
@@ -2,8 +2,10 @@ package com.gibuhagae.gibuhagae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gibuhagae")
 public class GibuhagaeApplication {
 
 	public static void main(String[] args) {
